0f67cd92bb06abe7bcd79fc4dfa8296e7d9470a9
Ahmed Nader <ahmednader5012@gmail.com>
2025-05-01 21:07:12 +0300

    final 2

---
+++ app/favicon.ico.tsx
@@ -0,0 +1,33 @@
+import { ImageResponse } from 'next/og';
+
+export const size = {
+  width: 32,
+  height: 32,
+};
+
+export const contentType = 'image/x-icon';
+
+export default function Icon() {
+  return new ImageResponse(
+    (
+      <div
+        style={{
+          fontSize: 24,
+          background: 'black',
+          width: '100%',
+          height: '100%',
+          display: 'flex',
+          alignItems: 'center',
+          justifyContent: 'center',
+          color: '#8b0000',
+          fontWeight: 700,
+        }}
+      >
+        H
+      </div>
+    ),
+    {
+      ...size,
+    }
+  );
+} 
